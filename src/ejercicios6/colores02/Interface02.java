22133099cf0d3a4ffc6a44ecb4235269f96be468
Jose Antonio Andreu <jose.andreu.r@gmail.com>
2019-03-22 19:26:24 +0100

    Terminada app Interfaces02

--- src/ejercicios6/colores02/Interface02.java
+++ src/ejercicios6/colores02/Interface02.java
@@ -6,16 +6,22 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import javax.swing.*;
 
+/*
+SE TRATA DE UNA PEQUEÑA APP QUE MUESTRA TRES BOTONES Y UNA ETIQUETA
+Cada botón representa un color dentro del sistema RGB
+Dependiendo del botón pulsado, podremos cambiar el color del panel con la rueda del ratón
+ */
+
 public class Interface02 extends JFrame {
 
-    public JLabel etiqueta;
-    public JButton botonR, botonG, botonB;
-    public JPanel panel;
-    public int contadorR, contadorG, contadorB;
-
-    public enum Pulsado {PULSADOR, PULSADOG, PULSADOB, NADA}
-
-    Pulsado pulsado;
+    private JLabel etiqueta;
+    private JButton botonR, botonG, botonB;
+    private JPanel panel;
+    private int contadorR, contadorG, contadorB;
+    // se utiliza para saber cual ha sido el último botón pulsado
+    private enum Pulsado {PULSADOR, PULSADOG, PULSADOB, NADA}
+    // variable de tipo Pulsado donde guardamos el enum
+    private Pulsado pulsado;
 
     public Interface02() {
         setSize(600, 400);
@@ -25,10 +31,11 @@ public class Interface02 extends JFrame {
     }
 
     private void iniciarComponentes() {
-        // inicializar
+        // inicio de los contadores de color
         contadorR = 0;
         contadorG = 0;
         contadorB = 0;
+        // inicio del enum
         pulsado = Pulsado.NADA;
 
         // configurar panel
@@ -43,6 +50,81 @@ public class Interface02 extends JFrame {
         etiqueta.setBounds(100, 50, 400, 50);
         panel.add(etiqueta);
 
+        setButtons();
+
+        // ahora vamos a meter un listener en todo el frame para la rueda del ratón
+        this.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                mouseWheelMoveListener(e);
+            }
+        });
+
+    }
+
+    private void setButtons() {
+        // botones, se indican tal y como deben aparecer, añadimos el listener de cada uno
+        botonR = new JButton("R");
+        botonR.setBounds(100, 200, 100,100);
+        panel.add(botonR);
+
+        botonR.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pulsado = Pulsado.PULSADOR;
+            }
+        });
+
+        botonG = new JButton("G");
+        botonG.setBounds(250,200,100,100);
+        panel.add(botonG);
+
+        botonG.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pulsado = Pulsado.PULSADOG;
+            }
+        });
+
+        botonB = new JButton("B");
+        botonB.setBounds(400,200,100,100);
+        panel.add(botonB);
+
+        botonB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pulsado = Pulsado.PULSADOB;
+            }
+        });
+    }
+
+    private void mouseWheelMoveListener(MouseWheelEvent e) {
+        if (pulsado != Pulsado.NADA) {
+            if (pulsado == Pulsado.PULSADOR) {
+                // aumentar o disminuir valor de R
+                contadorR += e.getWheelRotation();
+                // controlamos que no pase de 255 y que no baje del 0
+                if (contadorR < 0 || contadorR > 255) {
+                    // como no sabemos si estamos sumando o restando, de esta forma si suma lo resta y si lo resta lo suma
+                    contadorR -= e.getWheelRotation();
+                }
+            } else if (pulsado == Pulsado.PULSADOG) {
+                // aumentar o disminuir valor de G
+                contadorG += e.getWheelRotation();
+                if (contadorG < 0 || contadorG> 255) {
+                    contadorG -= e.getWheelRotation();
+                }
+            } else {
+                // aumentar o disminuir valor de B
+                contadorB += e.getWheelRotation();
+                if (contadorB < 0 || contadorB > 255) {
+                    contadorB -= e.getWheelRotation();
+                }
+            }
+        }
+        // aplicamos dinámicamente los valores al texto de la etiqueta
+        etiqueta.setText("Color (R: " + contadorR + ", G: " + contadorG + " , B: " + contadorB + ")");
+        panel.setBackground(new Color(contadorR, contadorG, contadorB));
     }
 
     public static void main(String[] args) {
