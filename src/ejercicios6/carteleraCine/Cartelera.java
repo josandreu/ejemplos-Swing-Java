cbf0b48cb0d783d097c6baa8e2d36f9ea75ebdb4
Jose Antonio Andreu <jose.andreu.r@gmail.com>
2019-03-22 21:50:47 +0100

    "Cartelera finalizada"

--- src/ejercicios6/carteleraCine/Cartelera.java
+++ src/ejercicios6/carteleraCine/Cartelera.java
@@ -24,15 +24,20 @@ public class Cartelera extends JFrame {
     private JScrollPane scrollPanel;
     // peliculas
     private Pelicula[] peliculas;
-
     // etiquetas
     private JLabel image, title, type;
+    // tamaño ventana
+    private int size_x = 480, size_y = 640;
+    // panel film
+    private int film_x = 460, film_y = 260;
+    // imagen
+    private int image_x = 180, image_y = 260;
 
     public Cartelera()  {
         this.setTitle("Cartelera de cine");
         Toolkit window = Toolkit.getDefaultToolkit();
         Dimension sizeWindow = window.getScreenSize();
-        this.setSize(480, 640);
+        this.setSize(size_x, size_y);
         // lo colocamos en el centro, más o menos
         this.setLocation(sizeWindow.width/3, sizeWindow.height/4);
         initComponents();
@@ -41,7 +46,7 @@ public class Cartelera extends JFrame {
     private void initComponents() {
         // panel principal
         mainPanel = new JPanel();
-        mainPanel.setBackground(Color.red);
+        mainPanel.setBackground(Color.white);
         // ponemos las peliculas una debajo de otra
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         this.add(mainPanel);
@@ -72,29 +77,30 @@ public class Cartelera extends JFrame {
     private void createPanels() {
         // panel pelicula
         filmPanel = new JPanel();
-        filmPanel.setBackground(Color.cyan);
+        filmPanel.setBackground(Color.white);
         // queremos que el texto y la imagen estén siempre uno al lado de otro
-        filmPanel.setLayout(new BoxLayout(filmPanel, BoxLayout.X_AXIS));
+        filmPanel.setLayout(new BorderLayout());
         mainPanel.add(filmPanel);
 
         // panel imagen
         imagePanel = new JPanel();
-        imagePanel.setBackground(Color.blue);
-        imagePanel.setPreferredSize(new Dimension(180,260));
-        filmPanel.add(imagePanel);
+        imagePanel.setBackground(Color.WHITE);
+        filmPanel.add(imagePanel, BorderLayout.WEST);
 
         // panel textos
         textPanel = new JPanel();
+        textPanel.setBackground(Color.WHITE);
         textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
-        textPanel.setBackground(Color.white);
-        filmPanel.add(textPanel);
+        // se restan 20 pixels para que el texto se separe un poco de la imagen
+        textPanel.setPreferredSize(new Dimension(film_x - image_x - 20, film_y));
+        filmPanel.add(textPanel, BorderLayout.EAST);
     }
 
     // método encargado de crear el contenido interior de las peliculas
     private void createContent(Pelicula pelicula) {
         // imagen de la pelicula, creamos objeto ImageIcon y recuperamos la imagen asignada en el constructor de la pelicula
         ImageIcon imageIcon = new ImageIcon(pelicula.getImagen());
-        Icon icon = new ImageIcon(imageIcon.getImage().getScaledInstance(180,260,Image.SCALE_SMOOTH));
+        Icon icon = new ImageIcon(imageIcon.getImage().getScaledInstance(image_x,image_y,Image.SCALE_SMOOTH));
 
         // etiqueta con la imagen
         image = new JLabel();
